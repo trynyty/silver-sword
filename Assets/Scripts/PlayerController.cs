6066a91529c4036e8e6fba430c4f2540c8aff79d
danko <danzhor@gmail.com>
2020-03-16 13:12:09 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        print(" move x, y: " + x + ", " + y);
+
+    }
+}
